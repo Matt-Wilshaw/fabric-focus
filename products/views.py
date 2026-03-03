1898f9dd2e521ba24d74243109399183a6825757
matt-wilshaw <m.wilshaw@outlook.com>
2026-03-03 20:24:26 +0000

    Align header, bag, profile, and toast template behavior

--- products/views.py
+++ products/views.py
@@ -55,7 +55,7 @@ def all_products(request):
         'current_sorting': current_sorting,
     }
 
-    return render(request, 'products/includes/products.html', context)
+    return render(request, 'products/products.html', context)
 
 
 def product_detail(request, product_id):
@@ -67,7 +67,7 @@ def product_detail(request, product_id):
         'product': product,
     }
 
-    return render(request, 'products/includes/product_detail.html', context)
+    return render(request, 'products/product_detail.html', context)
 
 
 def add_product(request):
@@ -83,9 +83,33 @@ def add_product(request):
     else:
         form = ProductForm()
         
-    template = 'products/includes/add_product.html'
+    template = 'products/add_product.html'
     context = {
         'form': form,
+    }
+
+    return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
     }
 
     return render(request, template, context)
