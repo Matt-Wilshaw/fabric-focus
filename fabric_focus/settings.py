25b7344bc83b05f75bbb479db9f662d8fd490c18
matt-wilshaw <m.wilshaw@outlook.com>
2026-03-19 10:42:33 +0000

    Fix success toast template syntax causing post-add product 500

--- fabric_focus/settings.py
+++ fabric_focus/settings.py
@@ -66,7 +66,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'fabric_focus.middleware.ExceptionLoggingMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
