e8f67aca548c0145355c99ab1dccbec170fa838c
matt-wilshaw <m.wilshaw@outlook.com>
2026-03-19 09:52:00 +0000

    Use process_exception for production traceback logging

--- fabric_focus/middleware.py
+++ fabric_focus/middleware.py
@@ -1,32 +1,29 @@
 """Project middleware utilities."""
 
 import traceback
+from django.utils.deprecation import MiddlewareMixin
 
 
-class ExceptionLoggingMiddleware:
+class ExceptionLoggingMiddleware(MiddlewareMixin):
     """Log unhandled exceptions with request context to stdout/stderr."""
 
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
+    def process_exception(self, request, exception):
+        bag = None
         try:
-            return self.get_response(request)
+            bag = request.session.get('bag', {})
         except Exception:
-            bag = None
-            try:
-                bag = request.session.get('bag', {})
-            except Exception:
-                bag = '<unavailable>'
+            bag = '<unavailable>'
 
-            print(
-                'UNHANDLED_EXCEPTION',
-                {
-                    'method': request.method,
-                    'path': request.path,
-                    'bag_type': type(bag).__name__,
-                    'bag_preview': str(bag)[:500],
-                },
-            )
-            print(traceback.format_exc())
-            raise
+        print(
+            'UNHANDLED_EXCEPTION',
+            {
+                'method': request.method,
+                'path': request.path,
+                'bag_type': type(bag).__name__,
+                'bag_preview': str(bag)[:500],
+                'error_type': type(exception).__name__,
+                'error': str(exception),
+            },
+        )
+        print(traceback.format_exc())
+        return None
