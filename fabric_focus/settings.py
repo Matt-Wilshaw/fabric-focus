b19446d268f52cb06659d8f5c8548784a817811c
matt-wilshaw <m.wilshaw@outlook.com>
2026-04-29 08:12:53 +0100

    Fix email backend for Python 3.12

--- fabric_focus/settings.py
+++ fabric_focus/settings.py
@@ -256,7 +256,7 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD') or os.getenv('EMAIL_HOST_PASS', '')
 
 if EMAIL_HOST_USER and EMAIL_HOST_PASSWORD:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_BACKEND = 'custom_email_backend.EmailBackend'
     EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', '1') == '1'
     EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', '0') == '1'
     EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
